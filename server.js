a7b3826a618d7007f787aea453d0c22528dedc29
Victor <Victor@Victors-MacBook-Pro.local>
2019-02-02 17:16:03 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs'); //how you use the handlebars (hbs) module
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server log', log + '\n', (error) => {
+        if(error) {
+            console.log('unable to append to server.log')
+        }
+    })
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenence.hbs');
+// }) this stops the website working and showing maintenance message
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (word) => {
+    return word.toUpperCase();
+})  
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', { 
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    })
+});
+
+app.get('/about' , (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errormesage: 'poop'
+    });
+});
+
+app.listen(3001, () => {
+    console.log('server is up on 3001')
+});
+
